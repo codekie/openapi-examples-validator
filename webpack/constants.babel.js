97b489319bdb3536450d690762c030ef8ac235c7
Code Kie <codekie@users.noreply.github.com>
2017-01-13 12:55:47 +0100

    set up initial dependencies and tasks

---
+++ webpack/constants.babel.js
@@ -0,0 +1,6 @@
+// WEBPACK BUILD CONSTANTS
+export const JS_REGEX = /\.js$|\.jsx$|\.es6$|\.babel$/;
+export const EXCLUDE_REGEX = /node_modules/;
+
+export const PROJECT_ROOT = __dirname + '/../';
+
