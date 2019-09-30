d8c316f69bad3eb4a7d165a15a5000c36e2ac1fb
Code Kie <codekie@users.noreply.github.com>
2019-09-30 08:52:31 +0200

    chore: add test-data for "single external example"-test

---
+++ test/data/output-single-external-example-valid.js
@@ -0,0 +1,11 @@
+/* eslint-disable max-len */
+module.exports = {
+    text: `Validating single external example
+Response schemas with examples found: 1
+Response examples without schema found: 0
+Total examples found: 1
+
+No errors found.
+
+`
+};
