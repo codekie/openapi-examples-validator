991c4c809cc8b182e01332da40fd23904e1a77a6
Code Kie <codekie@users.noreply.github.com>
2020-10-28 19:52:43 +0100

    fix: upgrade dependencies (#109)
    * chore: upgrade patch and minor versions
    
    * chore: upgrade stryker
    
    * chore: upgrade `commander`
    
    * chore: upgrade `webpack` and `webpack-cli`
    
    * chore: upgrade dependencies with `npm audit fix`

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,29 +1,9 @@
-module.exports = function(config) {
-    config.set({
-        mutate: [
-            'src/**/*.js'
-        ],
-        mutator: 'javascript',
-        packageManager: 'npm',
-        reporters: ['html', 'clear-text', 'progress', 'dashboard'],
-        testRunner: 'mocha',
-        transpilers: ['babel'],
-        testFramework: 'mocha',
-        coverageAnalysis: 'off',
-        babel: {
-            optionsFile: '.babelrc'
-        },
-        mochaOptions: {
-            // Optional mocha options
-            spec: ['./test/specs/**/*.js'],
-            config: '.mocharc.json',
-            package: 'package.json',
-            ui: 'bdd',
-            timeout: 3000,
-            require: [
-                '@babel/register',
-                './test/util/setup-tests'
-            ]
-        }
-    });
+module.exports = {
+    mutate: [
+        'src/**/*.js'
+    ],
+    packageManager: 'npm',
+    reporters: ['html', 'clear-text', 'progress', 'dashboard'],
+    testRunner: 'mocha',
+    coverageAnalysis: 'perTest'
 };
