ad92336928bca690495500a1e02b8feaeea2f79e
DL6ER <dl6er@dl6er.de>
2021-02-03 11:08:31 +0100

    Add tests for prepareOpenapiSpec and validateExampleInline
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- test/specs/index.js
+++ test/specs/index.js
@@ -1,7 +1,14 @@
 // IMPORTS
 
 const path = require('path'),
-    { validateFile, validateExample, 'default': validateExamples, validateExamplesByMap } = require('../../src'),
+    {
+        validateFile,
+        validateExample,
+        'default': validateExamples,
+        validateExamplesByMap,
+        prepareOpenapiSpec,
+        validateExampleInline
+    } = require('../../src'),
     {
         loadTestData,
         getPathOfTestData
@@ -117,6 +124,128 @@ describe('Main API', function() {
                     })
                 ]);
             });
+        });
+    });
+    describe('prepareOpenapiSpec', function() {
+        describe('be able to load v2.0 file', () => {
+            it('without errors', async() => {
+                (await prepareOpenapiSpec(getPathOfTestData('v2/valid-single-example'))).swagger
+                    .should.deep.equal("2.0");
+            });
+        });
+        describe('be able to load v3.0 file', () => {
+            it('without errors', async() => {
+                (await prepareOpenapiSpec(getPathOfTestData('v3/simple-api-with-example'))).openapi
+                    .should.deep.equal("3.0.0");
+            });
+        });
+        describe('should throw errors, when the files can\'t be found:', function() {
+            it('The schema-file', async() => {
+                const result = await prepareOpenapiSpec(FILE_PATH__NOT_EXISTS);
+                result.valid.should.equal(false);
+                result.errors.should.deep.equal([
+                    new ApplicationError(ErrorType.jsENOENT, {
+                        message: `ENOENT: no such file or directory, open '${ FILE_PATH__NOT_EXISTS }'`,
+                        params: {
+                            path: FILE_PATH__NOT_EXISTS
+                        }
+                    })
+                ]);
+            });
+        });
+    });
+    describe('validateExampleInline', function() {
+        const example1 = {
+            "versions": [{
+                "status": "CURRENT",
+                "updated": "2016-01-21T11:33:21Z",
+                "id": "v1.0",
+                "links": [{
+                    "href": "http://127.0.0.1:8774/v1/",
+                    "rel": "self"
+                }]
+            }]
+        };
+        const example2 = {
+            "versions": [{
+                "status": "CURRENT",
+                "updated": "2016-01-21T11:33:21Z",
+                "id": 1.0,
+                "links": [{
+                    "href": "http://127.0.0.1:8774/v1/",
+                    "rel": "self"
+                }]
+            }]
+        };
+        it('be able to validate example without errors', async() => {
+            const openApiSpec = await prepareOpenapiSpec(getPathOfTestData('v2/valid-single-example'));
+            const pathSchema = "$.paths./.get.responses.200.schema";
+            const result = await validateExampleInline(
+                    openApiSpec,
+                    pathSchema,
+                    example1);
+            result.valid.should.equal(true);
+        });
+        it('when the response-schema cannot be found', async() => {
+            const openApiSpec = await prepareOpenapiSpec(getPathOfTestData('v2/valid-single-example'));
+            const pathSchema = "$.paths./.get.responses.401.schema";
+            const result = await validateExampleInline(
+                    openApiSpec,
+                    pathSchema,
+                    example1);
+            result.valid.should.equal(false);
+            result.errors.should.deep.equal([
+                new ApplicationError(ErrorType.jsonPathNotFound, {
+                    message: `Path to schema can't be found: '${pathSchema}'`,
+                    params: {
+                        path: pathSchema
+                    },
+                    type: 'JsonPathNotFound'
+                })
+            ]);
+        });
+        it('throw error on example missing required property', async() => {
+            const openApiSpec = await prepareOpenapiSpec(getPathOfTestData('v2/valid-single-example'));
+            const pathSchema = "$.paths./.get.responses.200.schema";
+            const result = await validateExampleInline(
+                    openApiSpec,
+                    pathSchema,
+                    example2);
+            result.valid.should.equal(false);
+            result.errors.should.deep.equal([new ApplicationError(ErrorType.validation, {
+                dataPath: '.versions[0].id',
+                keyword: 'type',
+                message: 'should be string',
+                params: {
+                    type: 'string'
+                },
+                schemaPath: '#/properties/versions/items/properties/id/type',
+                exampleFilePath: 'inline'
+            })]);
+        });
+        it('handle multipe examples on the same openApiSpec object', async() => {
+            const openApiSpec = await prepareOpenapiSpec(getPathOfTestData('v2/valid-single-example'));
+            const pathSchema = "$.paths./.get.responses.200.schema";
+            const result1 = await validateExampleInline(
+                    openApiSpec,
+                    pathSchema,
+                    example1);
+            result1.valid.should.equal(true);
+            const result2 = await validateExampleInline(
+                    openApiSpec,
+                    pathSchema,
+                    example2);
+            result2.valid.should.equal(false);
+            result2.errors.should.deep.equal([new ApplicationError(ErrorType.validation, {
+                dataPath: '.versions[0].id',
+                keyword: 'type',
+                message: 'should be string',
+                params: {
+                    type: 'string'
+                },
+                schemaPath: '#/properties/versions/items/properties/id/type',
+                exampleFilePath: 'inline'
+            })]);
         });
     });
 });
